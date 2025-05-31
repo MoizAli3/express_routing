5fe9a5fdd9ee54def950d76cf7db85bed2ce39cb
Moiz Ali <moizk5590@gmail.com>
2025-05-31 15:18:52 +0500

    .

--- index.js
+++ index.js
@@ -6,18 +6,23 @@ const port = process.env.PORT || 3000;
 
 app.use("/api", router);
 
-app.use("/", express.static("public"));
+// app.use("/", express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Routes</h1>`);
+  res.send(`<h1>Routes</h1> <div> <br>
+        <a href="/api">API Pages</a> <br>
+        <a href="/api/home">Home Page</a> <br>
+        <a href="/api/users">Users Page</a> <br>
+        <a href="/api/products">Products Page</a> <br>
+    </div> `);
 });
 
 app.get("/api", (req, res) => {
-  res.status(200).send(`<h1>Welcome to API</h1>`);
+  res.status(200).send(`<h1>Welcome to API</h1> `);
 });
 
 app.use((req, res) => {
-  res.status(404).json({ status: 404 , error: "Not Found" });
+  res.status(404).json({ status: 404, error: "Not Found" });
 });
 
 app.listen(port, () => {
