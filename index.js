0d9bac2e5d6bfd1b6af74a062a50b17daf0aaf8d
Moiz Ali <moizk5590@gmail.com>
2025-05-31 15:12:12 +0500

    .

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/api", router);
+
 app.use("/", express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Routes</h1>`);
-});
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Routes</h1>`);
+// });
 
 app.get("/api", (req, res) => {
   res.status(200).send(`<h1>Welcome to API</h1>`);
